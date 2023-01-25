132f671bc03beb1cf2ebaf7f1cecd1d14642b6b6
Andrew MacIsaac <amacisaac95@gmail.com>
2023-01-25 16:51:52 -0700

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(drivers, foo){
+    
+    return drivers.filter((drivers) => drivers.toLowerCase().includes(foo.toLowerCase())); 
+}
+function fuzzyMatch(drivers, foo){
+    return drivers.filter((drivers) => drivers.slice(0,2) === foo);
+}
+function matchName(drivers, foo){
+    return drivers.filter((drivers) => drivers.name === foo) ;
+}
